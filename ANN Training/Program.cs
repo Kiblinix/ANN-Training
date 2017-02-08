5ac5e06360df52cfa0048c986f3432245066c755
Kiblinix <JackAMoore94@gmail.com>
2017-02-08 17:58:17 +0000

    Moved perceptron learning into it's own function, added proper training data

--- ANN Training/Program.cs
+++ ANN Training/Program.cs
@@ -5,39 +5,16 @@ class Program
     static void Main(string[] args)
     {
         double[][] trainingData = {
-            new double[] { 1, 1, 8, 1 },
-            new double[] { 1, 6, 2, -1 }
+            new double[] { 1, 1, 4, -1 },
+            new double[] { 1, 2, 9, 1 },
+            new double[] { 1, 5, 6, 1 },
+            new double[] { 1, 4, 5, 1 },
+            new double[] { 1, 6, 0.7, -1 },
+            new double[] { 1, 1, 1.5, -1 },
         };
         double[] weights = { 0, 0, 0 };
-        double threshold = 0;
 
-        bool failedTest = false;
-        do
-        {
-            failedTest = false;
-
-            for (int i = 0; i < trainingData.Length; i++)
-            {
-                double[] row = trainingData[i];
-                double S = (weights[0] * row[0]) + (weights[1] * row[1]) + (weights[2] * row[2]);
-                double output;
-
-                if (S > threshold)
-                    output = 1;
-                else if (S < threshold)
-                    output = -1;
-                else
-                    output = 0;
-
-                if (output != row[3])
-                {
-                    failedTest = true;
-                    weights[0] = weights[0] + (row[3] * row[0]);
-                    weights[1] = weights[1] + (row[3] * row[1]);
-                    weights[2] = weights[2] + (row[3] * row[2]);
-                }
-            }
-        } while (failedTest);
+        PerceptronLearning(trainingData, weights);
 
         Console.WriteLine("");
         Console.WriteLine("Bias: " + weights[0]);
@@ -46,5 +23,44 @@ class Program
         Console.WriteLine("");
         Console.WriteLine("Press any key to exit.");
         Console.Read();
+    }
+
+    static double ThresholdFunction(double sum, double threshold)
+    {
+        double output;
+
+        if (sum > threshold)
+            output = 1;
+        else if (sum < threshold)
+            output = -1;
+        else
+            output = 0;
+
+        return output;
+    }
+
+    static void PerceptronLearning(double[][] trainingData, double[] weights)
+    {
+        double threshold = 0;
+        bool failedTest = false;
+
+        do
+        {
+            failedTest = false;
+
+            for (int i = 0; i < trainingData.Length; i++)
+            {
+                double[] row = trainingData[i];
+                double S = (weights[0] * row[0]) + (weights[1] * row[1]) + (weights[2] * row[2]);
+
+                if (ThresholdFunction(S, threshold) != row[3])
+                {
+                    failedTest = true;
+                    weights[0] = weights[0] + (row[3] * row[0]);
+                    weights[1] = weights[1] + (row[3] * row[1]);
+                    weights[2] = weights[2] + (row[3] * row[2]);
+                }
+            }
+        } while (failedTest);
     }
 }
