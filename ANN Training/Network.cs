918e05f6c2b8699c3541850da2039149171f880d
Kiblinix <JackAMoore94@gmail.com>
2017-02-25 16:36:29 +0000

    Calculating RMSE on actual de-normalised outputs

--- ANN Training/Network.cs
+++ ANN Training/Network.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
 
 class Network
 {
@@ -12,10 +13,18 @@ class Network
     private Node outputNode = new Node();
 
     private List<List<double>> data = new List<List<double>>();
+    private List<List<double>> trainingSet = new List<List<double>>();
+    private List<List<double>> validationSet = new List<List<double>>();
+    private List<List<double>> testSet = new List<List<double>>();
+
     private int numHiddenNodes;
     private int numCycles;
     private double stepSize;
-    private int numInputs;    
+    private int numInputs;
+
+    // Used for de-normalising the output.
+    private double outputColumnMin;
+    private double outputColumnMax;
 
     public Network(int numHiddenNodes, int numCycles, double stepSize)
     {
@@ -28,6 +37,7 @@ class Network
     {
         ReadData("../../CWDataStudent.txt");
         NormaliseData();
+        SplitData();
 
         InitialiseNetwork();
         TrainNetwork();
@@ -45,21 +55,21 @@ class Network
         for (int i = 0; i < numHiddenNodes; i++)
         {
             Node hiddenNode = new Node();
-            hiddenNode.Bias = RandomiseWeight(numInputs);
+            hiddenNode.Bias = RandomiseWeight();
 
             for (var j = 0; j < inputLayer.Count; j++)
             {
-                Weight inputWeight = new Weight(inputLayer[j], RandomiseWeight(numInputs));
+                Weight inputWeight = new Weight(inputLayer[j], RandomiseWeight());
                 hiddenNode.inputs.Add(inputWeight);
             }
 
             hiddenLayer.Add(hiddenNode);
         }
         
-        outputNode.Bias = RandomiseWeight(numInputs);
+        outputNode.Bias = RandomiseWeight();
         for (var i = 0; i < hiddenLayer.Count; i++)
         {
-            Weight inputWeight = new Weight(hiddenLayer[i], RandomiseWeight(numInputs));
+            Weight inputWeight = new Weight(hiddenLayer[i], RandomiseWeight());
             outputNode.inputs.Add(inputWeight);
         }
     }
@@ -68,9 +78,9 @@ class Network
     {
         for (int n = 0; n < numCycles; n++)
         {
-            for (int i = 0; i < data.Count; i++)
+            for (int i = 0; i < trainingSet.Count; i++)
             {
-                List<double> row = data[i];
+                List<double> row = trainingSet[i];
 
                 // Set input nodes to input values
                 for (var j = 0; j < inputLayer.Count; j++)
@@ -102,10 +112,12 @@ class Network
     }
 
     private void TestNetwork()
-    {
-        for (int i = 0; i < data.Count; i++)
+    {       
+        double totalError = 0;
+
+        for (int i = 0; i < testSet.Count; i++)
         {
-            List<double> row = data[i];
+            List<double> row = testSet[i];
 
             // Set input nodes to input values
             for (var j = 0; j < inputLayer.Count; j++)
@@ -121,10 +133,21 @@ class Network
 
             // Forward pass to output node
             outputNode.CalculateOutput();
+
+            double predictedOutput = ((outputNode.Output - 0.1) / 0.8) * (outputColumnMax - outputColumnMin) + outputColumnMin;
+            double correctOutput = ((row[row.Count - 1] - 0.1) / 0.8) * (outputColumnMax - outputColumnMin) + outputColumnMin;
+
+            Debug.Print(predictedOutput + " \t " + correctOutput);
+
+            totalError += Math.Pow((predictedOutput - correctOutput), 2);
         }
+
+        // Calculated Root Mean Squared Error
+        double RMSE = Math.Sqrt(totalError / testSet.Count);
+        Console.WriteLine(RMSE);
     }
 
-    private double RandomiseWeight(int numInputs)
+    private double RandomiseWeight()
     {
         // Randomise weight/bias based on number of inputs between -2/n and +2/n
         return (rand.NextDouble() * 2 / numInputs) - 2 / numInputs;
@@ -151,7 +174,7 @@ class Network
                         failed = true;
                     }
 
-                    if (i != 4)   // Ignore columns 2 and 3 as they have low correlation
+                    //if (i != 4)   // Ignore columns 2 and 3 as they have low correlation
                         inputs.Add(field);
                 }
 
@@ -177,6 +200,12 @@ class Network
                 double columnMax = data.Max(u => u[j]);
                 double columnMin = data.Min(u => u[j]);
 
+                if (j == row.Count - 1)
+                {
+                    outputColumnMax = columnMax;
+                    outputColumnMin = columnMin;
+                }
+
                 // Normalise between 0.1 and 0.9
                 newRow.Add(((row[j] - columnMin) / (columnMax - columnMin)) * 0.8 + 0.1);
             }
@@ -185,5 +214,16 @@ class Network
         }
 
         data = normalisedData;
+    }
+
+    private void SplitData()
+    {
+        int trainingAmount = (int)(0.6 * data.Count);
+        int validationAmount = (data.Count - trainingAmount) / 2;
+        int testAmount = data.Count - trainingAmount - validationAmount;
+
+        trainingSet = data.GetRange(0, trainingAmount);
+        validationSet = data.GetRange(trainingAmount, validationAmount);
+        testSet = data.GetRange(validationAmount, testAmount);
     }
 }
